de3be5250c759877175ea1725dea64000794bd3a
Nathan Hunt <neighthan.hunt@gmail.com>
2020-12-17 01:40:17 -0500

    Improve RPN_to_eq.
    Use list instead of numpy array, refactor long `if` sequences to dictionaries.

--- aifeynman/RPN_to_eq.py
+++ aifeynman/RPN_to_eq.py
@@ -1,109 +1,54 @@
 # Turns an RPN expression to normal mathematical notation
 
-import numpy as np
+_VARIABLES = {
+    "0": "0",
+    "1": "1",
+    "P": "pi",
+    "a": "x0",
+    "b": "x1",
+    "c": "x2",
+    "d": "x3",
+    "e": "x4",
+    "f": "x5",
+    "g": "x6",
+    "h": "x7",
+    "i": "x8",
+    "j": "x9",
+    "k": "x10",
+    "l": "x11",
+    "m": "x12",
+    "n": "x13",
+}
+_OPS_UNARY = {
+    ">": "({}+1)",
+    "<": "({}-1)",
+    "~": "(-{})",
+    "\\": "({})**(-1)",
+    "L": "log({})",
+    "E": "exp({})",
+    "S": "sin({})",
+    "C": "cos({})",
+    "A": "abs({})",
+    "N": "asin({})",
+    "T": "atan({})",
+    "R": "sqrt({})",
+    "O": "(2*({}))",
+    "J": "(2*({})+1)",
+}
+_OPS_BINARY = set("+*-/")
 
 
-def RPN_to_eq(expr):
-
-    variables = [
-        "0",
-        "1",
-        "a",
-        "b",
-        "c",
-        "d",
-        "e",
-        "f",
-        "g",
-        "h",
-        "i",
-        "j",
-        "k",
-        "l",
-        "m",
-        "n",
-        "P",
-    ]
-    operations_1 = [
-        ">",
-        "<",
-        "~",
-        "\\",
-        "L",
-        "E",
-        "S",
-        "C",
-        "A",
-        "N",
-        "T",
-        "R",
-        "O",
-        "J",
-    ]
-    operations_2 = ["+", "*", "-", "/"]
-
-    stack = np.array([])
+def RPN_to_eq(expr: str) -> str:
+    stack = []
 
     for i in expr:
-        if i in variables:
-            if i == "P":
-                stack = np.append(stack, "pi")
-            elif i == "0":
-                stack = np.append(stack, "0")
-            elif i == "1":
-                stack = np.append(stack, "1")
-            else:
-                stack = np.append(stack, "x" + str(ord(i) - 97))
-        elif i in operations_2:
-            a1 = stack[-1]
-            a2 = stack[-2]
-            stack = np.delete(stack, -1)
-            stack = np.delete(stack, -1)
-            a = "(" + a2 + i + a1 + ")"
-            stack = np.append(stack, a)
-        elif i in operations_1:
-            a = stack[-1]
-            stack = np.delete(stack, -1)
-            if i == ">":
-                a = "(" + a + "+1)"
-                stack = np.append(stack, a)
-            if i == "<":
-                a = "(" + a + "-1)"
-                stack = np.append(stack, a)
-            if i == "~":
-                a = "(-" + a + ")"
-                stack = np.append(stack, a)
-            if i == "\\":
-                a = "(" + a + ")**(-1)"
-                stack = np.append(stack, a)
-            if i == "L":
-                a = "log(" + a + ")"
-                stack = np.append(stack, a)
-            if i == "E":
-                a = "exp(" + a + ")"
-                stack = np.append(stack, a)
-            if i == "S":
-                a = "sin(" + a + ")"
-                stack = np.append(stack, a)
-            if i == "C":
-                a = "cos(" + a + ")"
-                stack = np.append(stack, a)
-            if i == "A":
-                a = "abs(" + a + ")"
-                stack = np.append(stack, a)
-            if i == "N":
-                a = "asin(" + a + ")"
-                stack = np.append(stack, a)
-            if i == "T":
-                a = "atan(" + a + ")"
-                stack = np.append(stack, a)
-            if i == "R":
-                a = "sqrt(" + a + ")"
-                stack = np.append(stack, a)
-            if i == "O":
-                a = "(2*(" + a + "))"
-                stack = np.append(stack, a)
-            if i == "J":
-                a = "(2*(" + a + ")+1)"
-                stack = np.append(stack, a)
+        if i in _VARIABLES:
+            stack.append(_VARIABLES[i])
+        elif i in _OPS_BINARY:
+            a1 = stack.pop()
+            a2 = stack.pop()
+            stack.append(f"({a2}{i}{a1})")
+        elif i in _OPS_UNARY:
+            a = stack.pop()
+            stack.append(_OPS_UNARY[i].format(a))
     return stack[0]
